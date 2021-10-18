e0ec1b6bbc61eb47e5066e2e5bac136484993a44
Seema Shukla <Seema Shukla@Seema_Shukla>
2021-10-18 19:10:41 +0530

    updated

--- gJE/src/test/java/ETOE/gJE/Testcase.java
+++ gJE/src/test/java/ETOE/gJE/Testcase.java
@@ -1,12 +1,10 @@
 package ETOE.gJE;
 
-import org.junit.Test;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.testng.annotations.Test;
 public class Testcase {
 	
 	
--- gJE/src/test/java/ETOE/gJE/Testcase3.java
+++ gJE/src/test/java/ETOE/gJE/Testcase3.java
@@ -1,11 +1,12 @@
 package ETOE.gJE;
 
-import org.junit.Test;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 public class Testcase3 {
 	
